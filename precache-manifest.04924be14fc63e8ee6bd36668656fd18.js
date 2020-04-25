a2b70feeffbb2d5ca324b9e5a03e20ae90554c0a
Kara-Walla <kara.walla@teschglobal.com>
2020-04-25 16:26:28 -0500

    Updates

---
+++ precache-manifest.04924be14fc63e8ee6bd36668656fd18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "320055b7fadb693b5b5cf0326e2dd72b",
+    "url": "/family-site/index.html"
+  },
+  {
+    "revision": "1cafda1784ab7e4cd1dc",
+    "url": "/family-site/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6b7d8fd58ae3cff4a9e4",
+    "url": "/family-site/static/js/2.c4e9b1fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/family-site/static/js/2.c4e9b1fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cafda1784ab7e4cd1dc",
+    "url": "/family-site/static/js/main.1b4ca88e.chunk.js"
+  },
+  {
+    "revision": "7d44a373a700f62feccf",
+    "url": "/family-site/static/js/runtime-main.fd8e9b53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/family-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
